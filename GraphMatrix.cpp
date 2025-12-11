e0978e655cfb64a34f11a857ae0650116c006446
Shawn Dcosta <shawndcosta@Shawns-MacBook-Pro-4.local>
2025-12-10 20:57:28 -0800

    final

--- GraphMatrix.cpp
+++ GraphMatrix.cpp
@@ -1,15 +1,33 @@
 #include "GraphMatrix.hpp"
 
-GraphMatrix::GraphMatrix(int numV, GType type) {
+GraphMatrix::GraphMatrix(int numV, GType type) : Graph(numV, 0, type) {
     // TODO
+
+    adjMatrix = new bool*[v];
+    for (int i = 0; i < v;i++){
+        adjMatrix[i] = new bool[v];
+        for(int j = 0; j < v; j++){
+            adjMatrix[i][j] = false;
+        }
+    }   
+
 }
 
 GraphMatrix::~GraphMatrix() {
     // TODO
+    for (int i = 0; i < v; i++) {
+        delete[] adjMatrix[i];
+    }
+    delete[] adjMatrix;
 }
 
 bool GraphMatrix::isEdge(int i, int j) const {
     // TODO
+    if (i < 0 || i >= v || j < 0 || j >= v) {
+        return false;
+    }
+    
+    return adjMatrix[i][j] != 0;
 }
 
 ostream& GraphMatrix::print(ostream& os) const {
@@ -38,4 +56,13 @@ ostream& GraphMatrix::print(ostream& os) const {
 
 void GraphMatrix::toggleEdge(int i, int j) {
     // TODO
-}
+    if (i < 0 || i >= v || j < 0 || j >= v) {
+        return;  
+    }
+    if (graphType == DIRECTED) {
+        adjMatrix[i][j] = !adjMatrix[i][j];
+    } else {
+        adjMatrix[i][j] = !adjMatrix[i][j];
+        adjMatrix[j][i] = adjMatrix[i][j];  
+        }
+    }
